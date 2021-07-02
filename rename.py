c241b026cc4df1f41a713638037f030f48653b76
James Bradbury <jamesbradbury93@gmail.com>
2021-07-02 11:11:07 +0100

    make rename script work on b files

--- rename.py
+++ rename.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 import os
 
-sounds = [x for x in Path(".").rglob('*.wav')]
+sounds = [x for x in Path('b').rglob('*.aif')]
 
 for i, x in enumerate(sounds):
-    os.rename(x, f'static/sounds/{i}.wav')
-    
+    os.rename(x, f'static/sounds/{i+1}.aif')
